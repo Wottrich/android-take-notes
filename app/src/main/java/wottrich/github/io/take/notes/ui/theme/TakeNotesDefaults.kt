da2ff5143e59bc2f88bf028745ea504228514dcb
Wottrich <lucas.wottrich@ifood.com.br>
2024-04-08 19:33:01 -0300

    Create take note default colors

---
+++ app/src/main/java/wottrich/github/io/take/notes/ui/theme/TakeNotesDefaults.kt
@@ -0,0 +1,32 @@
+package wottrich.github.io.take.notes.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+fun darkColors(
+    primary: Color = ColorsDarkPallet.primary,
+    primaryVariant: Color = ColorsDarkPallet.primaryVariant,
+    secondary: Color = ColorsDarkPallet.secondary,
+    secondaryVariant: Color = ColorsDarkPallet.secondaryVariant,
+    background: Color = ColorsDarkPallet.background,
+    surface: Color = ColorsDarkPallet.surface,
+    error: Color = ColorsDarkPallet.error,
+    onPrimary: Color = ColorsDarkPallet.onPrimary,
+    onSecondary: Color = ColorsDarkPallet.onSecondary,
+    onBackground: Color = ColorsDarkPallet.onBackground,
+    onSurface: Color = ColorsDarkPallet.onSurface,
+    onError: Color = ColorsDarkPallet.onError
+): TakeNotesColors = TakeNotesColors(
+    primary,
+    primaryVariant,
+    secondary,
+    secondaryVariant,
+    background,
+    surface,
+    error,
+    onPrimary,
+    onSecondary,
+    onBackground,
+    onSurface,
+    onError,
+    isLight = false
+)
