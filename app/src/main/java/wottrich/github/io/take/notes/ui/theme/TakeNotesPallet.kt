d4994a61e481b937cd328c6ba5d5ddc98bba5fdf
Wottrich <lucas.wottrich@ifood.com.br>
2024-04-08 18:58:57 -0300

    Change theme name and to use only dark mode

---
+++ app/src/main/java/wottrich/github/io/take/notes/ui/theme/TakeNotesPallet.kt
@@ -0,0 +1,11 @@
+package wottrich.github.io.take.notes.ui.theme
+
+import androidx.compose.material3.darkColorScheme
+
+object TakeNotesPallet {
+    val DarkColorScheme = darkColorScheme(
+        primary = Purple80,
+        secondary = PurpleGrey80,
+        tertiary = Pink80
+    )
+}
